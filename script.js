4e6002ede09637cfe681c25dbbc2fffd336c8950
caiosss <caiobarros1712@gmail.com>
2024-07-08 22:36:43 -0300

    Ajustes no CSS e no HTML

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const temperaturaValue = document.querySelector("#temperatura .value")
 const chuvaValue = document.querySelector("#chuva .value")
 const climaValue = document.querySelector("#clima .value")
 const diaValue = document.querySelector("#dia .value")
+const humidadeValue = document.querySelector("#humidade .value")
 const dayIcon = document.querySelector("#dayIcon")
+const dateTime = document.querySelector(".date-time")
 const api_key = "os5UexDzEhGD7d0hGaAq78I1hRXxLvwH"
 
 botao.addEventListener('click',async () => {
@@ -12,15 +14,21 @@ botao.addEventListener('click',async () => {
     const resultado = await currentConditions(value)
     console.log(resultado)
 
+    const time = formatarData(resultado.LocalObservationDateTime)
+
     temperaturaValue.textContent = ''
     chuvaValue.textContent = ''
     climaValue.textContent = ''
     diaValue.textContent = ''
+    dateTime.textContent = ''
+    humidadeValue.textContent = ''
 
     temperaturaValue.append(resultado.Temperature.Metric.Value," ", resultado.Temperature.Metric.Unit)
     chuvaValue.append(resultado.HasPrecipitation === true ? "Sim" : "Não")
     climaValue.append(resultado.WeatherText)
     diaValue.append(resultado.IsDayTime === true ? "Sim" : "Não")
+    dateTime.append(time)
+    humidadeValue.append(resultado.RelativeHumidity, "%")
 
 
     if(resultado.IsDayTime){
@@ -41,11 +49,28 @@ async function requesitarDados(value) {
 
 async function currentConditions(value) {
     const key = await requesitarDados(value)
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/${key.Key}?apikey=${api_key}&language=pt-br`
+    const url = `http://dataservice.accuweather.com/currentconditions/v1/${key.Key}?apikey=${api_key}&language=pt-br&details=true`
     
     const response = await fetch(url)
     const data = await response.json()
     const [objeto] = data
     return objeto
+}
+
+
+
+function formatarData(dateTime) {
+    const time = new Date(dateTime)
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short'
+    }
+
+    return new Intl.DateTimeFormat('pt-BR', options).format(time)
 }
 
